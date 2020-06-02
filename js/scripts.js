e802c0fef26737d524604a1b1b2d129521a41fc3
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-02 14:12:09 -0700

    add two more definitions
    Co-authored-by: Pfulcher26 <pfulcher26@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
     $("p").click(function() {
       $(".definition-showing").toggle();
     });
-  
-  $("p3").click(function() {
+
+    $("p3").click(function() {
       $(".definition-showing2").toggle();
     });
-  });
+
+    $("p5").click(function() {
+    $(".definition-showing3").toggle();
+    });
+
+});
