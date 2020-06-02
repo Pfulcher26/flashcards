65aaa1f7cd757b95f3b318079ecf01259114a160
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-02 14:04:50 -0700

    add initial html and js to flashcard project.
    Co-authored by:Pfulcher26 <pfulcher26@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("p").click(function() {
+      $(".definition-showing").toggle();
+    });
+  
+  $("p3").click(function() {
+      $(".definition-showing2").toggle();
+    });
+  });
